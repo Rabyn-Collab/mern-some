d8faf840cfd718ec82ff685887848d4aa5bfba5a
Rabyn-Collab <babynshrestha64@gmail.com>
2024-09-26 20:10:43 +0545

    ch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import { NavLink } from "react-router-dom"
+import { FaBars } from "react-icons/fa6";
 
 const Header = () => {
   return (
-    <div className="bg-black text-white flex items-baseline px-4 py-2 justify-between sm:bg-red-800 md:bg-green-700 lg:bg-purple-700">
+    <div className="bg-black text-white flex items-baseline px-4 py-2 justify-between ">
+
       <h1 className="text-2xl">Head Logo</h1>
-      <nav className="space-x-5">
+      <FaBars className="hidden sm:flex" />
+      <nav className="space-x-5 sm:hidden">
         <NavLink to={'/about-page'}>About</NavLink>
         <NavLink to={'/contact-page'}>Contact</NavLink>
       </nav>
