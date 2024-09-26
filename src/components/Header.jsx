62a6787697153dd4833c5224f072db6f2525f1d3
Rabyn-Collab <babynshrestha64@gmail.com>
2024-09-26 20:57:22 +0545

    ch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,38 @@
 import { NavLink } from "react-router-dom"
-import { FaBars } from "react-icons/fa6";
+import { FaBars, FaClosedCaptioning, FaCross } from "react-icons/fa6";
+import { useState } from "react";
+import { FaCrown } from "react-icons/fa";
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  const handleShow = () => {
+    setShow(!show);
+  }
   return (
     <div className="bg-black text-white flex items-baseline px-4 py-2 justify-between ">
 
-      <h1 className="text-2xl">Head Logo</h1>
-      <FaBars className="hidden sm:flex" />
+      <div>
+        <h1 className="text-2xl">Head Logo</h1>
+        {show && <nav className="space-y-1 hidden mt-2 sm:flex flex-col">
+          <NavLink to={'/about-page'}>About</NavLink>
+          <NavLink to={'/contact-page'}>Contact</NavLink>
+        </nav>}
+      </div>
+
+
+
+      <button onClick={handleShow}>
+
+        {show ? <FaCrown className="hidden sm:flex" /> : <FaBars className="hidden sm:flex" />}
+      </button>
+
+
       <nav className="space-x-5 sm:hidden">
         <NavLink to={'/about-page'}>About</NavLink>
         <NavLink to={'/contact-page'}>Contact</NavLink>
       </nav>
+
+
     </div>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,15 @@
+import Button from "../components/Button"
+
 const HomePage = () => {
 
   return (
-    <div >
+    <div className="p-4 space-y-5">
 
 
-
+      <Button color='black' label='primary' />
+      <Button color='red' label='secondary' />
+      <Button color='green' label='warning' />
+      <Button color='blue' label='greeny' />
 
     </div>
   )
